0529932e58f593ca05a7d224f637e25e3789ef15
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-09-10 11:57:44 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+secret_word="Offworld"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses= False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        # print("Guess Count: " + guess_count )
+        guess = input("Enter Guess: ")
+        guess_count += 1
+    else: 
+        out_of_guesses = True
+
+if out_of_guesses: 
+    print("Out of guesses, You Lose")
+else: 
+    print("You Win")
